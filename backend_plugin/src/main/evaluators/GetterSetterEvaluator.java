186d948c0fcac28c214051c6316a18df0281b6e8
John Barcellos <johnbar@cs.washington.edu>
2019-03-10 10:53:05 -0700

    Cleaned up initial code

--- backend_plugin/src/main/evaluators/GetterSetterEvaluator.java
+++ backend_plugin/src/main/evaluators/GetterSetterEvaluator.java
@@ -1,46 +1,23 @@
 package main.evaluators;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTParser;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.VariableDeclaration;
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.DocumentEvent;
 import org.eclipse.jface.text.IDocument;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
-import org.eclipse.core.resources.IContainer;
-import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IResource;
-import org.eclipse.core.resources.IWorkspace;
-import org.eclipse.core.resources.IWorkspaceDescription;
-import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.jdt.core.IJavaProject;
-import org.eclipse.jdt.core.JavaCore;
-import org.eclipse.jdt.core.dom.AST;
-import org.eclipse.jdt.core.dom.ASTNode;
-import org.eclipse.jdt.core.dom.ASTParser;
-import org.eclipse.jdt.core.dom.AbstractTypeDeclaration;
-import org.eclipse.jdt.core.dom.BodyDeclaration;
-import org.eclipse.jdt.core.dom.CompilationUnit;
-import org.eclipse.jdt.core.dom.IVariableBinding;
-import org.eclipse.jdt.core.dom.MethodDeclaration;
-import org.eclipse.jdt.core.dom.VariableDeclaration;
-import org.eclipse.ui.IWorkbenchPage;
-import org.eclipse.ui.IWorkbenchPart;
-import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.handlers.HandlerUtil;
-import org.eclipse.ui.texteditor.ITextEditor;
-
 import main.ASTVisitors.VariableDeclarationFinder;
+import main.interfaces.FeatureID;
 
 /**
  * Evaluates DocumentEvent changes to determine if the user is manually creating getter and setter methods
- * for previously declared variables
+ * for previously declared variables. Parses the AST of a document whenever the user is manually typing
+ * new method declarations
  */
 public class GetterSetterEvaluator extends FeatureEvaluator {
 
@@ -51,105 +28,11 @@ public class GetterSetterEvaluator extends FeatureEvaluator {
      * @param document IDocument that this evaluator is attached to
      */
     public GetterSetterEvaluator(IDocument document) {
-	this.featureID = "getterSetterSuggestion";
+	this.featureID = FeatureID.GETTER_SETTER_FEATURE_ID;
 	this.document = document;
 	this.varNames = new HashSet<String>();
     }
 
-    // TODO: Delete this
-    // Left just for reference to grab any of the previous attempts
-    @Override
-    public boolean evaluateDocumentBeforeChange(DocumentEvent event) {
-
-//	IWorkspace workspace = ResourcesPlugin.getWorkspace();
-//	IWorkspaceDescription desc = workspace.getDescription();
-//
-//
-//	ASTParser parser = ASTParser.newParser(AST.JLS11);
-//	parser.setBindingsRecovery(true);
-//	parser.setSource(document.get().toCharArray());
-//	parser.setResolveBindings(true);
-//	CompilationUnit cu = (CompilationUnit)parser.createAST(null);
-//	cu.recordModifications();
-//
-//	VariableDeclarationFinder varFinder = new VariableDeclarationFinder();
-//
-//	cu.accept(varFinder);
-//	for(VariableDeclaration var : varFinder.getVariables()) {
-//	    System.out.println("Var name: " + var.getName());
-//	    System.out.println("Node Type: " + var.getNodeType());
-//	    System.out.println("Properties:");
-//	    Map m = var.properties();
-//	    for(Object p : m.keySet()) {
-//		System.out.println((String)p);
-//	    }
-//
-//	    IVariableBinding b =  var.resolveBinding();
-//	    if (b != null) {
-//		System.out.println("BinaryName: " + b.getName());
-//	    }
-//
-//
-//	}
-
-
-//	List<AbstractTypeDeclaration> types = cu.types();
-//	for(AbstractTypeDeclaration type : types) {
-//	    if(type.getNodeType() == ASTNode.TYPE_DECLARATION) {
-//	        // Class def found
-//	        List<BodyDeclaration> bodies = type.bodyDeclarations();
-//	        for(BodyDeclaration body : bodies) {
-//	            if(body.getNodeType() == ASTNode.METHOD_DECLARATION) {
-//	                MethodDeclaration method = (MethodDeclaration)body;
-//	                System.out.println("method declaration: ");
-//	                System.out.println("name: " + method.getName().getFullyQualifiedName());
-//	                System.out.println("modifiers: " + method.getModifiers());
-//	                System.out.println("return type: " + method.getReturnType2().toString());
-//	            }
-//	            if (body.getNodeType() == ASTNode.VARIABLE_DECLARATION_EXPRESSION) {}
-//	        }
-//	    }
-//	}
-
-
-
-//	IProject proj[] = workspace.getRoot().getProjects();
-//
-//	for(IProject p : proj) {
-//	    System.out.println("Project name: " + p.getName());
-//	    try {
-//		IJavaProject jpro = JavaCore.create(p);
-//		List<IFile> projectfiles = findAllProjectFiles(p);
-//		for (IResource res : projectfiles) {
-//		    System.out.println("Resource: " + res.getName());
-//		    if (res.getName().equals(textEditor.getTitle())) {
-//			System.out.println("Found a match!");
-//		    }
-//		}
-//
-//	    } catch (CoreException e) {
-//
-//	    }
-//	}
-
-//	if (proj != null) {
-//	    System.out.println("Project name: " + proj.getName());
-//	} else {
-//	    System.out.println("Project is null");
-//	}
-
-//	System.out.println("Workspace: ");
-//	System.out.println(workspace.toString());
-//	System.out.println("Workspace desc: ");
-//	System.out.println(desc.toString());
-
-	// Check if the user types
-	// public **** getVarName( || public ** get_varname(
-	// We only care about adding white space or removing characters
-	// Either of those cases will have a length of 0 once the string is trimmed
-
-	return false;
-    }
 
     /**
      * Checks if the event added a get or set method regarding a previously declared variable
@@ -175,13 +58,12 @@ public class GetterSetterEvaluator extends FeatureEvaluator {
 	    String lineText = document.get(lineOffset, lineLength).trim().toLowerCase();
 
 	    // check for public or protected at the start of the line
-	    if (lineText.startsWith("public") || lineText.startsWith("protected")) {
+	    if (lineText.startsWith("public ") || lineText.startsWith("protected ")) {
 		// If so, then update the variable names
 		// This operation is costly, so we limit it to only when it's necessary
 		updateKnownVariableNames();
 		return checkGetterOrSetter(lineText);
 	    }
-
 	} catch (BadLocationException e) {}
 	return false;
     }
@@ -191,13 +73,14 @@ public class GetterSetterEvaluator extends FeatureEvaluator {
      */
     private void updateKnownVariableNames() {
 
-	// clear out all previous var names
+	// clear out all previous variable names
 	this.varNames = new HashSet<String>();
 
+	// Create the AST through the ASTParser and find all variable declarations
 	ASTParser parser = ASTParser.newParser(AST.JLS11);
-	parser.setBindingsRecovery(true);
+//	parser.setBindingsRecovery(true);
 	parser.setSource(document.get().toCharArray());
-	parser.setResolveBindings(true);
+//	parser.setResolveBindings(true);
 	CompilationUnit cu = (CompilationUnit)parser.createAST(null);
 	cu.recordModifications();
 
@@ -205,28 +88,10 @@ public class GetterSetterEvaluator extends FeatureEvaluator {
 
 	cu.accept(varFinder);
 	for(VariableDeclaration var : varFinder.getVariables()) {
-//	    System.out.println("Var name: " + var.getName());
-//	    System.out.println("Var qualified name: " + var.getName().getFullyQualifiedName());
-//	    System.out.println("Var identifier: " + var.getName().getIdentifier());
 	    // Skip empty strings
 	    if (var.getName().toString().length() > 0) {
 		varNames.add(var.getName().toString().toLowerCase());
 	    }
-//	    System.out.println("Var Type: " + var.getName().getNodeType());
-//	    System.out.println("Var start position: " + var.getName().getStartPosition());
-//	    System.out.println("Node Type: " + var.getNodeType());
-//	    System.out.println("Properties:");
-//	    Map m = var.properties();
-//	    for(Object p : m.keySet()) {
-//		System.out.println((String)p);
-//	    }
-
-//	    IVariableBinding b =  var.resolveBinding();
-//	    if (b != null) {
-//		System.out.println("BinaryName: " + b.getName());
-//	    }
-
-
 	}
 
     }
@@ -260,20 +125,4 @@ public class GetterSetterEvaluator extends FeatureEvaluator {
 	}
 	return false;
     }
-
-    private List<IFile> findAllProjectFiles(IContainer container) throws CoreException {
-        IResource[] members = container.members();
-        List<IFile> list = new ArrayList<>();
-
-        for (IResource member : members) {
-            if (member instanceof IContainer) {
-                IContainer c = (IContainer) member;
-                list.addAll(findAllProjectFiles(c));
-            } else if (member instanceof IFile) {
-                list.add((IFile) member);
-            }
-        }
-        return list;
-    }
-
 }
