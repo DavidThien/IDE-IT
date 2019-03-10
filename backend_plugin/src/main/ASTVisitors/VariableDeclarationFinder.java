07dcb39696cf8cd6482533f79eac056bfd006b3f
John Barcellos <johnbar@cs.washington.edu>
2019-03-10 10:55:16 -0700

    Cleaned up white space

--- backend_plugin/src/main/ASTVisitors/VariableDeclarationFinder.java
+++ backend_plugin/src/main/ASTVisitors/VariableDeclarationFinder.java
@@ -12,21 +12,21 @@ import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
  * ASTVisitor class that finds any VariableDeclaration nodes in the AST
  */
 public final class VariableDeclarationFinder extends ASTVisitor {
-  private final List <VariableDeclaration> vars = new ArrayList <> ();
+  private final List<VariableDeclaration> vars = new ArrayList<>();
 
   /**
    * If any VariableDeclarationFragments are found in the AST, then save them
    */
   @Override
-  public boolean visit (VariableDeclarationFragment fragment) {
-    vars.add (fragment);
+  public boolean visit(VariableDeclarationFragment fragment) {
+    vars.add(fragment);
     return super.visit(fragment);
   }
 
   /**
    * @return an immutable list view of the variables discovered by this visitor
    */
-  public List <VariableDeclaration> getVariables() {
+  public List<VariableDeclaration> getVariables() {
     return Collections.unmodifiableList(vars);
   }
 }
