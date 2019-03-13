2d22cb7946aebd2273a12e11b4b6d8f8f2d41aa4
John Barcellos <johnbar@cs.washington.edu>
2019-03-12 19:13:02 -0700

    Updated script to run positive and negative use cases

--- backend_plugin/runTestCases.sh
+++ backend_plugin/runTestCases.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Run the mvn command to check against all of our test cases 
-mvn -Dtest=*PositiveCases surefire-report:report
+# Run the mvn command to check against all of our postive and negative test cases
+mvn -Dtest=*Cases surefire-report:report
 
 # Results can be found in target/site/surefire-report
